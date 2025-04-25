a52083f00c7c3cc85745edbe952304274665f808
maxkulbako <mkulbako@gmail.com>
2025-04-25 14:26:26 +0200

    fix: add debaging logic

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,13 @@ export const config = {
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
+  const { pathname } = new URL(req.url);
+
+  console.log("[🔐 Middleware] url=", pathname, " role=", token?.role);
+  console.log("[🔐 Middleware] token:", token);
   if (!token) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
-
-  const { pathname } = new URL(req.url);
 
   if (pathname.startsWith("/admin") && token.role !== "ADMIN") {
     return NextResponse.redirect(new URL("/unauthorized", req.url));
