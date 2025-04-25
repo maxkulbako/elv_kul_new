a144fca259354771cf052fd48f1c8cddca8254e6
maxkulbako <mkulbako@gmail.com>
2025-04-25 15:05:56 +0200

    chore: test deploy

--- middleware.ts
+++ middleware.ts
@@ -6,15 +6,26 @@ export const config = {
 };
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  });
 
   const { pathname } = new URL(req.url);
 
+  console.log("[Cookies]", req.headers.get("cookie"));
+
   console.log("[🔐 Middleware] url=", pathname, " role=", token?.role);
   console.log("[🔐 Middleware] token:", token);
+
   if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
+    // NextResponse.rewrite will leave you on /admin/*, but put the /sign-in content
+    return NextResponse.rewrite(new URL("/sign-in", req.url));
   }
+
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/sign-in", req.url));
+  // }
 
   if (pathname.startsWith("/admin") && token.role !== "ADMIN") {
     return NextResponse.redirect(new URL("/unauthorized", req.url));
